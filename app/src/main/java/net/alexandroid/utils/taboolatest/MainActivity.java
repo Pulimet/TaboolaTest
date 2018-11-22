8526662e6a65b35a0e0aa80712bc763543630c61
Alexey Korolev <korolev2009@gmail.com>
2018-11-22 08:19:08 +0200

    Set Taboola Widget in MainActivity + add custom tabs dependency

--- app/src/main/java/net/alexandroid/utils/taboolatest/MainActivity.java
+++ app/src/main/java/net/alexandroid/utils/taboolatest/MainActivity.java
@@ -3,11 +3,27 @@ package net.alexandroid.utils.taboolatest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.taboola.android.TaboolaWidget;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTaboolaWidget();
+    }
+
+    private void setTaboolaWidget() {
+        TaboolaWidget taboolaWidget = findViewById(R.id.taboolaWidget);
+
+        taboolaWidget.setPublisher("sdk-tester")
+                .setMode("thumbs-feed-01")
+                .setPlacement("Feed without video")
+                .setPageType("article")
+                .setPageUrl("https://www.ynet.co.il/articles/0,7340,L-5411684,00.html")
+                .setTargetType("mix");
+
+        taboolaWidget.fetchContent();
     }
 }
